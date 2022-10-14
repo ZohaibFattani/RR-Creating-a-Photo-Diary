b1f56798d365315344a8625911cfd3600133e441
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-13 20:41:58 -0700

    moved each function to individual file

--- bin.js
+++ bin.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const open = require('open')
 const { exec, spawn } = require('child_process')
+const isWin = process.platform === 'win32'
 exec('npx webpack --mode="development"', () => {
-    spawn('npx', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
+    spawn( isWin ? 'npx.cmd' : 'npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
--- src/index.js
+++ src/index.js
@@ -1,73 +1,12 @@
-
+import {koalas } from './koalas'
+import {selectKoala} from './select'
+import {render} from './render'
+import {renderKoalaList} from './renderList'
+import {renderKoalaCard} from './koalaCard'
 let activeKoala = null
 
-// Changes the active koala, then re-renders the page to display that koala
-let selectKoala = koala => {
-    activeKoala = koala
-    render()
-}
-
-// Called once when the page loads, and again every time a koala is selected
-let render = () => {
-    let oldPage=  document.querySelector('.koala-container')
-    let newPage;
-    if(activeKoala){
-        newPage = renderActiveKoala(activeKoala)
-    } else {
-        newPage = renderKoalaList(koalas)
-    }
-    newPage.classList.add('koala-container')
-    oldPage.replaceWith(newPage)
-}
 
 
-// Renders a list of koalas
-let renderKoalaList = koalas => {
-    let koalaContainer = document.createElement('div')
-
-    koalaContainer.setAttribute('class', 'ui items')
-
-    koalas.forEach( koala => {
-        let koalaCard = renderKoalaCard(koala)
-        koalaContainer.append(koalaCard)
-    })
-
-    return koalaContainer
-}
-
-// Renders a card to display a single koala
-let renderKoalaCard = koala => {
-    let koalaCard = document.createElement('div')
-    koalaCard.setAttribute('class', 'item')
-    koalaCard.style.cursor = 'pointer';
-    koalaCard.addEventListener('click', () => {
-        selectKoala(koala)
-    })
-
-    let koalaImage = renderKoalaCardImage(koala)
-    let koalaContent = renderKoalaCardContent(koala)
-
-    koalaCard.append(
-        koalaImage,
-        koalaContent
-    )
-
-    return koalaCard
-}
-
-// Renders the image of a koala card
-let renderKoalaCardImage = koala => {
-    let imageContainer = document.createElement('div')
-    let koalaImage = document.createElement('img')
-
-    imageContainer.setAttribute('class', 'image')
-
-    koalaImage.setAttribute('src', koala.imageURL)
-
-    imageContainer.append(koalaImage)
-
-    return imageContainer
-}
 
 // Renders the content of a koala card (their name and description)
 let renderKoalaCardContent = koala => {
